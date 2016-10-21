ca9afe6fe9c3cd9c95b86735a4b0a0766fb22494
Andres Araya <araya.andres@gmail.com>
2016-10-21 14:12:59 -0600

    delete all files before create the symbolic link

--- install.sh
+++ install.sh
@@ -3,11 +3,26 @@ PATH_TO_CONFIG_FILES=$1
 if [ -z "$PATH_TO_CONFIG_FILES" ]; then PATH_TO_CONFIG_FILES=$PWD; fi
 PREV_DIR=$PWD
 cd
+
+# bash
 echo "[ -f $PATH_TO_CONFIG_FILES/bash/bash_profile.txt ] && source $PATH_TO_CONFIG_FILES/bash/bash_profile.txt" >> ~/.bash_profile
+
+# git
+rm .gitconfig
 ln -s $PATH_TO_CONFIG_FILES/git/gitconfig.txt .gitconfig
+
+# octave
+rm .octaverc
 ln -s $PATH_TO_CONFIG_FILES/octave/octaverc.txt .octaverc
+
+# tmux
+rm .tmux.conf
 ln -s $PATH_TO_CONFIG_FILES/tmux/tmux_conf.txt .tmux.conf
+
+# nvim
+mkdir .vim_undo_dir
 mkdir .config
 cd .config
 ln -s $PATH_TO_CONFIG_FILES/nvim
+
 cd $PREV_DIR
