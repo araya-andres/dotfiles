30e482c797b073604510fe0a1cb2cdc03226bfdd
Andres Araya <andresaraya@Andress-MacBook-Pro.local>
2016-10-21 13:55:44 -0600

    append a line to .bash_profile to source bash_profile from the repo

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 PATH_TO_CONFIG_FILES=$1
-if [ -z "$PATH_TO_CONFIG_FILES" ]; then PATH_TO_CONFIG_FILES='.'; fi
+if [ -z "$PATH_TO_CONFIG_FILES" ]; then PATH_TO_CONFIG_FILES=$PWD; fi
 PREV_DIR=$PWD
 cd
+echo "[ -f $PATH_TO_CONFIG_FILES/bash/bash_profile.txt ] && source $PATH_TO_CONFIG_FILES/bash/bash_profile.txt" >> ~/.bash_profile
 ln -s $PATH_TO_CONFIG_FILES/git/gitconfig.txt .gitconfig
 ln -s $PATH_TO_CONFIG_FILES/octave/octaverc.txt .octaverc
 ln -s $PATH_TO_CONFIG_FILES/tmux/tmux_conf.txt .tmux.conf
