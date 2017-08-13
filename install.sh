40227cc3245807ebc650f55480ad81846ba088d0
Andres Araya <araya.andres@gmail.com>
2017-08-13 10:03:08 -0600

    Remove "txt" extension.

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ PREV_DIR=$PWD
 cd
 
 # bash
-echo "[ -f $PATH_TO_CONFIG_FILES/bash/bash_profile.txt ] && source $PATH_TO_CONFIG_FILES/bash/bash_profile.txt" >> ~/.bash_profile
+echo "[ -f $PATH_TO_CONFIG_FILES/bash/bash_profile ] && source $PATH_TO_CONFIG_FILES/bash/bash_profile" >> ~/.bash_profile
 
 # git
 rm .gitconfig
-ln -s $PATH_TO_CONFIG_FILES/git/gitconfig.txt .gitconfig
+ln -s $PATH_TO_CONFIG_FILES/git/gitconfig .gitconfig
 
 # octave
 rm .octaverc
-ln -s $PATH_TO_CONFIG_FILES/octave/octaverc.txt .octaverc
+ln -s $PATH_TO_CONFIG_FILES/octave/octaverc .octaverc
 
 # tmux
 rm .tmux.conf
