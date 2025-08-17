0b1ca7a52b4af036dc023549006787365c8c7027
Andres Araya <araya.andres@gmail.com>
2025-08-17 17:36:00 -0600

    Revert "nvim-tmux integration"
    This reverts commit c5903c1606f31fb94192d5bb020e9714fcfd1395.

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -6,8 +6,3 @@ vim.o.expandtab = true
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
 vim.o.tabstop = 4
-
-vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
--- lazyvim/lua/plugins/nvim-tmux-navigator.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"christoomey/vim-tmux-navigator",
-	vim.keymap.set("n", "<c-h>", ":TmuxNavigateLeft<CR>"),
-	vim.keymap.set("n", "<c-j>", ":TmuxNavigateDown<CR>"),
-	vim.keymap.set("n", "<c-k>", ":TmuxNavigateUp<CR>"),
-	vim.keymap.set("n", "<c-l>", ":TmuxNavigateRight<CR>"),
-}
