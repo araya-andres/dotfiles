e5886c679208a6c7ccefd304159f3a758fd79405
Andres Araya <araya.andres@gmail.com>
2025-08-17 17:32:11 -0600

    fix case

--- lazyvim/lua/plugins/nvim-tmux-navigator.lua
+++ lazyvim/lua/plugins/nvim-tmux-navigator.lua
@@ -1,7 +1,7 @@
 return {
 	"christoomey/vim-tmux-navigator",
-	vim.keymap.set("n", "<c-h>", ":TmuxNavigateLeft<CR>"),
-	vim.keymap.set("n", "<c-j>", ":TmuxNavigateDown<CR>"),
-	vim.keymap.set("n", "<c-k>", ":TmuxNavigateUp<CR>"),
-	vim.keymap.set("n", "<c-l>", ":TmuxNavigateRight<CR>"),
+	vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>"),
+	vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>"),
+	vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>"),
+	vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>"),
 }
