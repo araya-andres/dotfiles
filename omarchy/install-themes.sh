9dcd2772f13baca5ad31238a89a302313ab2632a
Andres Araya <araya.andres@gmail.com>
2025-08-19 10:28:50 -0600

    format script

--- omarchy/install-themes.sh
+++ omarchy/install-themes.sh
@@ -1,1 +1,3 @@
-wget -qO- https://manuals.omamix.org/2/the-omarchy-manual/90/extra-themes | sed -n 's#.*href="\(https://github.com[^"]*\)".*#\1#p' | xargs -n1 omarchy-theme-install
+wget -qO- https://manuals.omamix.org/2/the-omarchy-manual/90/extra-themes |
+    sed -n 's#.*href="\(https://github.com[^"]*\)".*#\1#p' |
+    xargs -n1 omarchy-theme-install
