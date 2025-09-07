913da01c33b151ae628d2226d02fe9a9cf3a60a0
Andres Araya <araya.andres@gmail.com>
2025-09-07 17:15:28 -0600

    use tab to navigate options, enter to select

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -11,6 +11,7 @@ return {
   {
     "hrsh7th/nvim-cmp",
     config = function()
+      local luasnip = require("luasnip")
       local cmp = require("cmp")
       require("luasnip.loaders.from_vscode").lazy_load()
       cmp.setup({
@@ -21,15 +22,41 @@ return {
         },
         window = {
           completion = cmp.config.window.bordered(),
-          -- documentation = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
         },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
+        mapping = {
+          ['<CR>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              if luasnip.expandable() then
+                luasnip.expand()
+              else
+                cmp.confirm({
+                  select = true,
+                })
+              end
+            else
+              fallback()
+            end
+          end),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+              luasnip.jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        },
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "luasnip" },
@@ -38,5 +65,5 @@ return {
         }),
       })
     end,
-  }
+  },
 }
