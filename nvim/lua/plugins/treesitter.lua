b58a1e7cf7409cf1131997ba32eae71447ddecdb
Andres Araya <araya.andres@gmail.com>
2025-09-07 11:55:18 -0600

    lazy vim config

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = { "c", "cpp", "rust" },
+      auto_install = true,
+      highlight = {
+        enable = true
+      }
+    })
+  end
+}
