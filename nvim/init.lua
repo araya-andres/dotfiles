90e348b3c65bd31ec193a89c1ba362f47bb10ddd
Andres Araya <araya.andres@gmail.com>
2025-09-07 13:56:00 -0600

    refactor vim options

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,2 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
-vim.cmd("set relativenumber")
-
+require("vim-options")
 require("config.lazy")
