8ae598cf3175bba98239a0b58a193d88ef458d27
Andres Araya <araya.andres@gmail.com>
2025-09-14 19:11:18 -0600

    [nvim] current line blame

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -9,5 +9,10 @@ return {
     },
     {
         "lewis6991/gitsigns.nvim",
+        config = function ()
+            require('gitsigns').setup {
+                current_line_blame = true,
+            }
+        end
     },
 }
