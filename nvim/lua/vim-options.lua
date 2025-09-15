575fe8fa2583ca8b69238a46058b9a677bc69013
Andres Araya <araya.andres@gmail.com>
2025-09-15 14:24:43 -0600

    [nvim] persistent undo / more options

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -5,7 +5,15 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set nowrap")
+
 vim.cmd("set showbreak=↪")
+
+vim.cmd("set splitright")
+vim.cmd("set splitbelow")
+
+vim.cmd("set undodir=$HOME/.vim_undo_dir")
+vim.cmd("set undofile")
+
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "cpp",
 	callback = function(event)
