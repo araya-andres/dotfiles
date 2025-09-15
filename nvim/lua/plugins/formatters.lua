b513498983a590304d828969b1eb817955a3ac85
Andres Araya <araya.andres@gmail.com>
2025-09-15 08:03:11 -0600

    [nvim] add lua formatter

--- nvim/lua/plugins/formatters.lua
+++ nvim/lua/plugins/formatters.lua
@@ -1,26 +1,26 @@
 return {
-    "stevearc/conform.nvim",
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                cpp = { "clang-format" },
-                c = { "clang-format" },
-            },
-            formatters = {
-                clang_format = {
-                    prepend_args = {
-                        "--style=file",
-                        "--fallback-style=LLVM",
-                    },
-                },
-            },
-        })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*",
-            callback = function(args)
-                require("conform").format({ bufnr = args.buf })
-            end,
-        })
-    end,
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				cpp = { "clang-format" },
+				c = { "clang-format" },
+				lua = { "stylua" },
+			},
+			formatters = {
+				clang_format = {
+					prepend_args = {
+						"--style=file",
+						"--fallback-style=LLVM",
+					},
+				},
+			},
+		})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
+			end,
+		})
+	end,
 }
-
