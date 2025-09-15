491c93a284bbe5fbf44a46c6acdda31102573beb
Andres Araya <araya.andres@gmail.com>
2025-09-15 12:34:36 -0600

    [nvim] dap configs
    configurations should be defined per project in ./vscode/launch.json

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -14,43 +14,6 @@ return {
 			command = "gdb",
 			args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
 		}
-		dap.configurations.c = {
-			{
-				name = "Launch",
-				type = "gdb",
-				request = "launch",
-				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				args = {}, -- provide arguments if needed
-				cwd = "${workspaceFolder}",
-				stopAtBeginningOfMainSubprogram = false,
-			},
-			{
-				name = "Select and attach to process",
-				type = "gdb",
-				request = "attach",
-				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				pid = function()
-					local name = vim.fn.input("Executable name (filter): ")
-					return require("dap.utils").pick_process({ filter = name })
-				end,
-				cwd = "${workspaceFolder}",
-			},
-			{
-				name = "Attach to gdbserver :1234",
-				type = "gdb",
-				request = "attach",
-				target = "localhost:1234",
-				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				cwd = "${workspaceFolder}",
-			},
-		}
-		dap.configurations.cpp = dap.configurations.c
 		vim.keymap.set("n", "<F5>", dap.continue)
 		vim.keymap.set("n", "<F9>", dap.toggle_breakpoint)
 		vim.keymap.set("n", "<F10>", dap.step_over)
