9235520c9bcb81944ee9199f3456aa3120dd3fc0
Andres Araya <araya.andres@gmail.com>
2025-09-15 21:09:32 -0600

    [lazyvim] disable plugins

--- lazyvim/lua/plugins/disabled.lua
+++ lazyvim/lua/plugins/disabled.lua
@@ -1,4 +1,8 @@
 return {
-  { "bufferline.nvim", enabled = false },
-  { "folke/flash.nvim", enabled = false },
+	{ "bufferline.nvim", enabled = false },
+	{ "flash.nvim", enabled = false },
+	{ "folke/flash.nvim", enabled = false },
+	{ "folke/which-key.nvim", enabled = false },
+	{ "grug-far.nvim", enabled = false },
+	{ "noice.nvim", enabled = false },
 }
