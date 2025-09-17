e14bbf711c6d11afc161dcb93cfdc6f68754942f
Andres Araya <araya.andres@gmail.com>
2025-09-16 21:27:09 -0600

    [lazyvim] add shortcuts for build / debug

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -2,4 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<F7>", ":CMakeBuild<CR>")
+vim.keymap.set("n", "<F5>", require("dap").continue)
+vim.keymap.set("n", "<F9>", require("dap").toggle_breakpoint)
+vim.keymap.set("n", "<F10>", require("dap").step_over)
+vim.keymap.set("n", "<F11>", require("dap").step_into)
+vim.keymap.set("n", "<Shift-F11>", require("dap").step_out)
--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -6,3 +6,12 @@ vim.o.expandtab = true
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
 vim.o.tabstop = 4
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "cpp",
+	callback = function(event)
+		vim.cmd('let &makeprg = "cd build && make"')
+		vim.keymap.set("n", "<leader><Space>", ":ClangdSwitchSourceHeader<Cr>")
+		vim.keymap.set("n", "<F7>", ":make<Cr>")
+	end,
+})
